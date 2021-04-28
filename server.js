6b9167984c0308708dcff917cd0581268b1e86e1
Tomas Pinto <tpinto@atixlabs.com>
2021-04-28 12:23:11 -0300

    fix: read accepted language from header object

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get("/api/hello", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/whoami", function (req, res) {
-  res.json({ipaddress: req.ip, language: req.acceptsLanguages, software: req.headers['user-agent']});
+  res.json({ipaddress: req.ip, language: req.headers['accept-language'], software: req.headers['user-agent']});
 });
 
 
